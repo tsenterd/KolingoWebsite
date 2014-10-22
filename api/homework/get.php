43a07c0d8ef504af7875196e2afba5e131d55610
Jonathan Galperin <jonathan@galperin.com>
2014-10-21 20:54:27 -0400

    Fixed API connection after moving files to a new directory.

--- api/homework/get.php
+++ api/homework/get.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-include "../connect.php";
+include "../../connect.php";
 
 header('Content-Type: application/json');
 
