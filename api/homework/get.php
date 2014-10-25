9cddaf2b06168333987f382063db6bfc0d7ffe9f
Jonathan Galperin <jonathan@galperin.com>
2014-10-25 14:04:41 -0400

    added date to api.

--- api/homework/get.php
+++ api/homework/get.php
@@ -10,8 +10,15 @@ header('Content-Type: application/json');
 
 if (isset($_GET["class_id"])) {
     $class_id = $_GET["class_id"];
-    $query = mysqli_query($conn, 'SELECT * FROM homework where class_id = '.$class_id);
+    if (isset($_GET["after"])) {
+        $date = urldecode($_GET["after"]);
+        $query = mysqli_query($conn, 'SELECT * FROM homework where class_id = '.$class_id.' and created > "'.$date.'"');
+    }
 } else {
+    if (isset($_GET["after"])) {
+        $date = urldecode($_GET["after"]);
+        $query = mysqli_query($conn, 'SELECT * FROM homework where created > "'.$date.'"');
+    }
     $query = mysqli_query($conn, 'SELECT * FROM homework');
 }
 
