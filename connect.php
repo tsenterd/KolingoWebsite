602784b6fd6236f3cf03a8516ef2beecb6b45457
David Tsenter <tsenterd@gmail.com>
2015-01-22 23:52:09 -0500

    updated db password

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 
 $database_host = "us-cdbr-iron-east-01.cleardb.net";
 $database_username = "b868297364dd46";
-$database_password = "08f3a3d5";
+$database_password = "17ac929f6e7d01d";
 $database_name = "heroku_20efd45a2eed6a8";
 
 
